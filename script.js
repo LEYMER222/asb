dff58da0f9251cceb9e29e4869b7f4a85958cacb
LEYMER222 <abdullohshaymanov222@gmail.com>
2025-03-14 20:05:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let nameK = prompt ('String typdagi malumot kiring')
+console.log(nameK)
+
+let number = prompt ('Number typdagi malumot kiring')
+console.log(number)
+
+let truee = prompt ('boolean typdagi malumot kiring')
+console.log(truee)
+
+let userResponse = confirm("hamma malumotlarni togri yozganingizgaishonchingiz komilmi?");
+// if (userResponse) {
+//     alert("Вы нажали ОК!");
+// } else {
+//     alert("Вы нажали Отмена!");
+// }
+alert (`Siz kiritgan ${nameK} sozi string typega tegishli 
+Siz kiritgan ${number} sozi number typega tegishli  
+Siz kiritgan ${nameK} sozi boolean typega tegishli`)
